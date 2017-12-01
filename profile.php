fd6fdb74400607575a10d67e104c844f64359665
reynojon <reynojon@oregonstate.edu>
2017-11-30 21:19:06 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -30,8 +30,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   }
 ?>
 
-<hr>
-
 <?php
 $C_username=$_SESSION['Stored_Username'];
 $sql="SELECT * FROM User WHERE username LIKE '$C_username' ";
@@ -53,7 +51,7 @@ $result=mysqli_query($conn,$sql);
 	<div class="formContainer">
 		<form name="frmUser" method="post" action="/cs362/profile.php" />
 				<p>
-					<h2>Take Survey</h2>
+					<h2>Take Self-Assessment</h2>
           <a href="http://khanshadid.com/cs362/survey.php">Click Here</a>
 				</p>
 				<p>
